e6136f3f00e679390052117c7b5750776d1574bd
Kassidy <kqssidy@gmail.com>
2022-02-17 21:29:45 -0600

    Setup ContentView and got API key

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inspires = [Inspire]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(inspires) { inspire in
+                NavigationLink(
+                    destination: Text(inspire.author)
+                        .padding(),
+                    label: {
+                        Text(inspire.quote)
+                    })
+            }
+            .navigationTitle("Inspiring Quotes")
+        }
+        .onAppear(perform: {
+            getInspired()
+        })
     }
+    func getInspired() {
+        let apiKey = "?rapidapi-key=dcdeb11b82msh92bdd760de21315p1ddd5djsnd15d938d1e5b"
+    }
+}
+
+struct Inspire: Identifiable {
+    let id = UUID()
+    var quote: String
+    var author: String
 }
 
 struct ContentView_Previews: PreviewProvider {
