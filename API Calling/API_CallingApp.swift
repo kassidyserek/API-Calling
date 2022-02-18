8f2b8f3365949ef3e83f29949d33fc5a76749b5a
Kassidy <kqssidy@gmail.com>
2022-02-17 21:08:43 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by KSerek on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
